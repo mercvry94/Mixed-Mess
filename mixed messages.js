8215d2fc70ec89c526b40394b6a365c321274b89
Asher G Vongtau <agv239@nyu.edu>
2022-08-25 10:05:49 -0400

    First Steps

---
+++ mixed messages.js
@@ -0,0 +1,5 @@
+let players = ['Messi', 'Ronaldo', 'Neymar', 'Mbappe', 'Lewandowski', 'Dembele', 'Benzema', 'Haaland', 'Vinnicus', 'Bale', 'Pulisic']
+
+let randomPlayer = players[Math.floor(Math.random() * players.length)];
+
+console.log(`${randomPlayer} loves the game of football`);
